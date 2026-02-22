5c35af79eb8c3fc964c5008e45dcc542145cd5e1
hakimsa <samouh591@gmail.com>
2026-02-22 20:31:56 +0100

    add modif

--- lib/pages/homePage.dart
+++ lib/pages/homePage.dart
@@ -66,7 +66,7 @@ class _Homepagetate extends State<Homepage> {
 ),)
     , SizedBox(height: 100,child: Row(
       mainAxisAlignment: MainAxisAlignment.spaceAround,
-      children: [Container(margin: EdgeInsets.all(15),width: 250,height: 225,color: const Color.fromARGB(255, 74, 4, 38),child: Text("descriptiiiion completa nuevo feature Production new tag recien en prod  rama main ",style: TextStyle(color: Colors.white),),),Container(width: 150,height: 225,color: const Color.fromARGB(255, 107, 107, 106),child: Text("ver mas Detalles aqui merge to main prod funciona "),),],
+      children: [Container(margin: EdgeInsets.all(15),width: 250,height: 225,color: const Color.fromARGB(255, 134, 209, 87),child: Text("descriptiiiion completa nuevo feature Production new tag recien en prod  rama main ",style: TextStyle(color: Colors.white),),),Container(width: 150,height: 225,color: const Color.fromARGB(255, 107, 107, 106),child: Text("ver mas Detalles aqui merge to main prod funciona correctamente el ultimo tag en ram nain"),),],
     ),)
      
       ],
