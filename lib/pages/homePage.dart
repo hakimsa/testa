78cd04e09a52b33428c8792ca8b18f1dd3a3743f
hakimsa <samouh591@gmail.com>
2026-02-22 22:12:32 +0100

    add

--- lib/pages/homePage.dart
+++ lib/pages/homePage.dart
@@ -64,12 +64,44 @@ class _Homepagetate extends State<Homepage> {
       },
     );
   },
-),)
-    , SizedBox(height: 100,child: Row(
+),),SizedBox.fromSize(size: Size.fromHeight(20),)
+    , Row(
       mainAxisAlignment: MainAxisAlignment.spaceAround,
-      children: [Container(margin: EdgeInsets.all(15),width: 250,height: 225,color: const Color.fromARGB(220, 149, 227, 4),child: Text("descriptiiiion completa nuevo feature Production new tag recien en prod  rama main ",style: TextStyle(color: Colors.white),),),Container(width: 150,height: 225,color: const Color.fromARGB(255, 107, 107, 106),child: Text("ver mas Detalles aqui merge to main prod funciona el new tag "),),],
-    ),)
-     
+      children: [
+        
+        Container(
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(15),
+           gradient: LinearGradient(
+            colors: [Color.fromARGB(220, 2, 69, 46), Color.fromARGB(220, 1, 57, 6)],
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight,
+          ),
+        ),
+        padding: EdgeInsets.all(15),width: 250,height: 225,child: Text("descriptiiiion completa nuevo feature Production new tag recien en prod  rama main ",style: TextStyle(color: Colors.white),),),
+         Container(
+          decoration: BoxDecoration(
+           borderRadius: BorderRadius.circular(15),
+          gradient: LinearGradient(
+            colors: [Color.fromARGB(220, 0, 0, 0), Color.fromARGB(220, 1, 65, 44)],
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight,
+          ),
+          ),
+        padding: EdgeInsets.all(15),width: 250,height: 225,child: Text("Description for  completa nuevo feature Production new tag recien en prod  rama main ",style: TextStyle(color: Colors.white),),)
+        ,Container(
+          decoration: BoxDecoration(
+           borderRadius: BorderRadius.circular(15),
+          gradient: LinearGradient(
+            colors: [Color.fromARGB(220, 1, 48, 32), Color.fromARGB(220, 1, 65, 44)],
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight,
+          ),
+          ),
+        padding: EdgeInsets.all(15),width: 250,height: 225,child: Text("Description for  completa nuevo feature Production new tag recien en prod  rama main ",style: TextStyle(color: Colors.white),),)
+        
+      ]),
+     Divider(color: Colors.indigo,)
       ],
      )
 
@@ -81,10 +113,11 @@ class _Homepagetate extends State<Homepage> {
  Card _cardlistEmployee(BuildContext context, List<Employe> employees, int index) {
 final employee = employees[index];
     return Card(
-      margin: EdgeInsets.only(top: 12.55),
+      margin: EdgeInsets.symmetric(vertical: 10.55,horizontal: 10),
           child: ListTile(
             title: Text(employee.name), // ajusta según tu modelo
             subtitle: Text(employee.email), 
+            contentPadding: EdgeInsets.all(10),
             leading: SizedBox(
   width: 45,
   height: 45,
@@ -105,7 +138,8 @@ final employee = employees[index];
     ),
   ),
 ),
-            trailing: Text(employee.jobTitle),// si existe
+            trailing: Text(employee.jobTitle),
+            textColor: const Color.fromARGB(221, 73, 72, 72),// si existe
           ),
         );
   }
